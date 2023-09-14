cee9b99360c4c9f32ed472a264aef6485dc8604b
Luminator1122 <next13.gen.coder@gmail.com>
2023-09-14 17:00:32 +0500

    addd question no. 8

--- question4.js
+++ question4.js
@@ -3,5 +3,5 @@
 //Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 //Albert Einstein once said, “A person who never made a mistake never tried anything new.”
 let author = "'M.Ali Jinnah'";
-let quote = "'No any power on earth can demolish Pakistan'";
+let quote = "'No any power on earth can demolish Pakistan.'";
 console.log(author, ", Once Said, ", quote);
