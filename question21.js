e4d6248c4bf0f7d5d1e0f7dd4ced2f2b99169ac7
Luminator1122 <next13.gen.coder@gmail.com>
2023-09-16 20:46:02 +0500

    question 21 added

--- question21.js
+++ question21.js
@@ -1,47 +1,45 @@
 "use strict";
-// //write a program create object and store  in the array
-// class Students {
-//     name:string;
-//     gender:string;
-//     rollNumber:number;
-//     class:number;
-//     session:string;
-// constructor (n:string, g:string, r:number, c:number, s:string)
-// {
-//     this.name=n
-//     this.gender=g
-//     this.rollNumber=r
-//     this.class=c
-//     this.session=s
-// }
-// // Here we create the method or function to have a good statement of the data and call it later
-// myFunction():string // myFunction is a self made name. u can select any name  
-// {
-//         // this is a statement to b output whenever this function runs/called
-//     return `Student's Name is ${this.name} and its Gender is ${this.gender} having Roll Number ${this.rollNumber} in the ${this.session} Session. `
-// }
-// }
-// // Now we give arguments  to  the function or  Method named myFunction as mentioned above
-// var Student1 = new Students("Asif", "Male", 110, 10, "Morning")  // new is predefied  word,
-// var Student2 = new Students("Adnan", "Male", 111, 10, "Evening")    // Students is class name
-// var Student3 = new Students("Kashif", "Male", 112, 10, "Night")
-// var Student4 = new Students("Zahid", "Male", 113, 10, "General")
-// // here we  create an empty  array in which we push the data to take output
-// var studentArray:Students[]=[]
-// studentArray.push(Student1)
-// studentArray.push(Student2)
-// studentArray.push(Student3)
-// studentArray.push(Student4)
-// //check one by one consoles
-// //console.log(Student1)
-// console.log(studentArray)
-// // to  change the name of any student ...give command as
-// // Student1.name="Zeeshan"
-// // console.log(Student1)
-// // when cosoled.. the Name Adnan is changed to Zeeshan
-// // call of function now
-// // console.log(Student1.myFunction()) 
-// // console.log(Student2.myFunction())
-// // console.log(Student3.myFunction())
-// // console.log(Student4.myFunction())
-// // this will result out put as "Student's Name is Asif and its Gender is Male having Roll Number 110 in the Morning Session. "
+// Question No.21
+// Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// 	• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False..
+let my_Places = {
+    Data_Darbar: 'lahore',
+    Badshahi_Mosque: 'lahore',
+    Shrine_of_Lal_Shahbaz_Qalandar: 'Sehwan Sharif Sindh',
+    Faisal_Mosque: 'Islamabad',
+    Blue_Mosque_Replica: 'Islamabad'
+};
+//test case-1
+console.log("Is Data_Darbar == lahore? MY prediction  TRUE");
+console.log(my_Places.Data_Darbar === 'lahore');
+//test case-2
+console.log("Is Data_Darbar == Faisalabad? MY prediction FALSE");
+console.log(my_Places.Data_Darbar === 'Faisalabad');
+//test case-3
+console.log("Is Shrine_of_Lal_Shahbaz_Qalandar == Sehwan Sharif Sindh? MY prediction  TRUE");
+console.log(my_Places.Shrine_of_Lal_Shahbaz_Qalandar === 'Sehwan Sharif Sindh');
+//test case-4
+console.log("Is Shrine_of_Lal_Shahbaz_Qalandar == Faisalabad? MY prediction FALSE");
+console.log(my_Places.Shrine_of_Lal_Shahbaz_Qalandar === 'Faisalabad');
+//test case-5
+console.log("Is Badshahi_Mosque == lahore? MY prediction  TRUE");
+console.log(my_Places.Badshahi_Mosque === 'lahore');
+//test case-6
+console.log("Is Badshahi_Mosque == Islamabad? MY prediction  FALSE");
+console.log(my_Places.Badshahi_Mosque === 'Islamabad');
+//test case-7
+console.log("Is Faisal_Mosque  == Islamabad? MY prediction  TRUE");
+console.log(my_Places.Faisal_Mosque === 'Islamabad');
+//test case-8
+console.log("Is Faisal_Mosque  == lahore? MY prediction  FALSE");
+console.log(my_Places.Faisal_Mosque === 'lahore');
+//test case-9
+console.log("Blue_Mosque_Replica == Islamabad? MY prediction TRUE");
+console.log(my_Places.Blue_Mosque_Replica === 'Islamabad');
+//test case-10
+console.log("Is Blue_Mosque_Replica == lahore? MY prediction FALSE");
+console.log(my_Places.Blue_Mosque_Replica === 'lahore');
