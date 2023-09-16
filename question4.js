73d7e30b7e54033f5cbd1067dc0a8d7cee26b08d
Luminator1122 <next13.gen.coder@gmail.com>
2023-09-16 14:22:05 +0500

    more question added

--- question4.js
+++ question4.js
@@ -2,6 +2,5 @@
 //Question No.4
 //Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 //Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-let author = "'M.Ali Jinnah'";
-let quote = "'No any power on earth can demolish Pakistan.'";
-console.log(author, ", Once Said, ", quote);
+let famous_quote = "'M. Ali Jinnah' once Said !,'No any power on earth can demolish Pakistan.'";
+console.log(famous_quote);
--- question4.ts
+++ question4.ts
@@ -1,6 +1,6 @@
 //Question No.4
 //Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
 //Albert Einstein once said, “A person who never made a mistake never tried anything new.”
-let author:string="'M.Ali Jinnah'"
-let quote:string="'No any power on earth can demolish Pakistan.'"
-console.log(author, ", Once Said, ", quote)
+
+let famous_quote:string="'M. Ali Jinnah' once Said !,'No any power on earth can demolish Pakistan.'"
+console.log(famous_quote)
