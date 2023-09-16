c676072b969b1fefd0a8db9ff45da71316e421c4
Luminator1122 <next13.gen.coder@gmail.com>
2023-09-16 20:16:41 +0500

    question 19 resolved

--- question19.ts
+++ question19.ts
@@ -1,5 +1,5 @@
 // Question No.19
-// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+// Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
 
 let mydiffPlaces:string[]=["K2","River Indus","Islamabad","Urdu","Pakistan"]
 for (let index = 0; index < mydiffPlaces.length; index++)
@@ -7,4 +7,17 @@ for (let index = 0; index < mydiffPlaces.length; index++)
     const element = mydiffPlaces[index];
     console.log(element)
 }
-//pending
+
+// *think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+var An_object_laptop = {
+   Data_Darbar : 'lahore',
+   Keyboard:"stylish",
+   Screen:"14 Inch",
+   Storage:"256 GB",
+   RAM:"8 GB",
+   Processor:"Core i5-2 Gen"
+}
+console.log(An_object_laptop)
+
+
